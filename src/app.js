27ba7700d9b8bb2b207c4f34baf5bfa5f9e010ae
Adithya <anandsaikrishnan.adithya@gmail.com>
2022-07-04 20:48:14 -0700

    added hover functionality to board

--- src/app.js
+++ src/app.js
@@ -2,19 +2,32 @@
 let dimension = 16
 // Create a 16x16 grid of square divs
 
-const container = document.querySelector('div.container')
-
-for (let i = 0; i < dimension; i++) {
-    const newRow = document.createElement('div')
-    newRow.className = 'row'
-
-    for (let j = 0; j < dimension; j++) {
-        const newCell = document.createElement('div')
-        const newId = i * dimension + j
-        newCell.className = 'cell'
-        newCell.id = `cell-${newId}`
-        newRow.appendChild(newCell)
-    }
-    container.appendChild(newRow)
+const hoverEvent = (e) => {
+    e.stopPropagation()
+    console.log(e);
+    document.getElementById(e.target.id).style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
 }
+
+const setDimensions = (newNumber = 16) => {
+    
+    dimension = newNumber
+    const board = document.querySelector('div#board')
+    board.innerHTML = ''
+    board.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${dimension}, 1fr)`
+    for (let i = 0; i < dimension; i++) {
+        for (let j = 0; j < dimension; j++) {
+            const newCell = document.createElement('div')
+            const newId = i * dimension + j
+            newCell.className = 'cell'
+            newCell.id = `cell-${newId}`
+            newCell.addEventListener('mouseover', hoverEvent)
+            board.appendChild(newCell)
+        }
+    }
+}
+
+//Initialize the board
+
+setDimensions() 
 
