80a5708e8a3b126ecb5cea650ac3e792f852a024
Adithya <anandsaikrishnan.adithya@gmail.com>
2022-07-04 23:44:10 -0700

    made further updates to etch-a-sketch, nearly done

--- src/app.js
+++ src/app.js
@@ -1,33 +1,59 @@
-
-let dimension = 16
+let dimension = 16;
 // Create a 16x16 grid of square divs
 
-const hoverEvent = (e) => {
-    e.stopPropagation()
-    console.log(e);
-    document.getElementById(e.target.id).style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
-}
+const pressEvent = (e) => {
+	e.stopPropagation();
+	document.getElementById(e.target.id).style.backgroundColor =
+		"#ada1eb";
+};
 
 const setDimensions = (newNumber = 16) => {
-    
-    dimension = newNumber
-    const board = document.querySelector('div#board')
-    board.innerHTML = ''
-    board.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`
-    board.style.gridTemplateRows = `repeat(${dimension}, 1fr)`
-    for (let i = 0; i < dimension; i++) {
-        for (let j = 0; j < dimension; j++) {
-            const newCell = document.createElement('div')
-            const newId = i * dimension + j
-            newCell.className = 'cell'
-            newCell.id = `cell-${newId}`
-            newCell.addEventListener('mouseover', hoverEvent)
-            board.appendChild(newCell)
-        }
-    }
-}
+	dimension = newNumber;
+	const board = document.querySelector("div#board");
+	board.innerHTML = "";
+	board.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
+	board.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
+	for (let i = 0; i < dimension; i++) {
+		for (let j = 0; j < dimension; j++) {
+			const newCell = document.createElement("div");
+			const newId = i * dimension + j;
+			newCell.className = "cell";
+			newCell.id = `cell-${newId}`;
+			newCell.addEventListener("mouseover", pressEvent);
 
+			if (newId === 0) {
+				newCell.style.borderRadius = "0.3vh 0px 0px 0px";
+			}
+
+			if (newId === dimension - 1) {
+				newCell.style.borderRadius = "0vh 0.3vh 0px 0px";
+			}
+
+			if (newId === dimension * (dimension - 1)) {
+				newCell.style.borderRadius = "0px 0px 0.3vh 0px";
+			}
+
+			if (i === dimension * dimension - 1) {
+				newCell.style.borderRadius = "0px 0px 0px 0.3vh";
+			}
+			board.appendChild(newCell);
+		}
+	}
+};
+
+const onChange = (e) => {
+	newDimension = e.target.value;
+	const label = document.querySelector("label#range-text");
+	label.innerText = `Dimension: ${newDimension} x ${newDimension}`;
+	setDimensions(newDimension);
+};
+
+const initialize = () => {
+	const slider = document.querySelector("input#range-slider");
+	slider.addEventListener("change", onChange);
+
+	setDimensions();
+};
 //Initialize the board
 
-setDimensions() 
-
+initialize();
